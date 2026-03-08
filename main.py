a9676698087978dd67c988c810320d35c9a1e12b
fepgptspsantos-eng <fepgptspsantos@gmail.com>
2026-03-08 06:49:50 -0300

    Implement basic Discord bot with lock/unlock commands
    Added Discord bot functionality with lock and unlock commands.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-NAME=LilicaBot
-TYPE=bot
-MAIN=main.py
-RAM=100
-AUTORESTART=true
-VERSION=latest
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Lilica online como {bot.user}')
+
+@bot.command()
+async def lock(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
+    await ctx.send("🔒 Este canal foi trancado pela Lilica!")
+
+@bot.command()
+async def unlock(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+    await ctx.send("🔓 Este canal foi liberado!")
+
+bot.run('COLE_SEU_TOKEN_AQUI')
+
